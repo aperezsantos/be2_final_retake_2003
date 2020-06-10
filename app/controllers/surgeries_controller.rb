50c3a7dfe5b1d1cb43a75e1205e974c1773e1cf5
Ana Perez Santos <a.jperez@yahoo.com>
2020-06-10 11:29:46 -0600

    US2 WIP

--- app/controllers/surgeries_controller.rb
+++ app/controllers/surgeries_controller.rb
@@ -4,6 +4,7 @@ class SurgeriesController < ApplicationController
   end
 
   def show
+    @surgeries = Surgery.all
     @surgery = Surgery.find(params[:id])
   end
 
--- spec/features/surgeries/show_spec.rb
+++ spec/features/surgeries/show_spec.rb
@@ -8,6 +8,8 @@ RSpec.describe "surgery show page", type: :feature do
       @doctor_2 = @unmh.doctors.create!(name: "T. Fernandes", specialty: "Oncology", education: "Harvard")
       @surgery_1 = Surgery.create!(title: "Knee Surgery", day: "Monday", room: "8")
       @surgery_2 = Surgery.create!(title: "Heart Surgery", day: "Monday", room: "3")
+      @surgery_3 = Surgery.create!(title: "Lung Surgery", day: "Monday", room: "6")
+      @surgery_3 = Surgery.create!(title: "Lung Surgery", day: "Thursday", room: "3")
       SurgeryDoctor.create!(doctor: @doctor_1, surgery: @surgery_2)
       SurgeryDoctor.create!(doctor: @doctor_2, surgery: @surgery_2)
 
@@ -26,11 +28,37 @@ RSpec.describe "surgery show page", type: :feature do
 
     it "i see the count of doctors on this surgery" do
       click_link("Heart Surgery")
-      
+
       expect(page).to have_content("Number of Doctors Operating: 2")
+    end
+
+    xit "i see a section with other surgeries happening the same day" do
+      click_link("Heart Surgery")
+
+      within("#surgeries-#{@surgery_2.id}") do
+        expect(page).to have_content("Other Surgeries Happening Today:")
+        expect(page).to have_content(@surgery_1.title)
+        expect(page).to have_content(@surgery_3.title)
+        expect(page).to_not have_content(@surgery_4.title)
+      end
+    end
+
+    it "I see " do
+
     end
   end
 end
+
+
+# User Story 3, Add a Doctor to a Surgery
+# As a visitor
+# When I visit a surgery's show page
+# I see a surgery's title, date, and
+# I see a field with instructions to "Add A Doctor To This Surgery"
+# When I input a doctor's unique id into that field
+# And click submit
+# I'm taken back to that surgery's show page
+# And I see the name of that doctor listed on the page
 
 
 
@@ -44,4 +72,5 @@ end
 # And I see the count of doctors that are on this surgery
 
 # And I see a section of the page that says "Other surgeries happening this day of the week:"
+
 # And under that header I see titles of all surgeries that happen on the same day of the week as this surgery.
